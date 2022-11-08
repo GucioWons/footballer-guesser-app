75009e4b76d9714095738f1c9f07621052ce42c0
GucioWons <wgutek1@wp.pl>
2022-11-08 12:13:38 +0100

    Sign up form validators

--- app/src/main/java/com/guciowons/footballer_guesser_app/SignUpActivity.java
+++ app/src/main/java/com/guciowons/footballer_guesser_app/SignUpActivity.java
@@ -5,18 +5,84 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.guciowons.footballer_guesser_app.helpers.StringHelper;
 
 public class SignUpActivity extends AppCompatActivity {
+    EditText username_edittext, email_edittext, password_edittext, confirm_password_edittext;
+    Button register_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        username_edittext = findViewById(R.id.username_edittext);
+        email_edittext = findViewById(R.id.email_edittext);
+        password_edittext = findViewById(R.id.password_edittext);
+        confirm_password_edittext = findViewById(R.id.confirm_password_edittext);
+
+        register_btn = findViewById(R.id.register_btn);
+        register_btn.setOnClickListener(view -> processRegisterForm());
     }
 
     public void goBack(View view){
         Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void processRegisterForm(){
+        if(!validateUserName() || !validateEmail() || !validatePassword()){
+            return;
+        }
+        Toast.makeText(SignUpActivity.this, "Success", Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean validateUserName(){
+        String username = username_edittext.getText().toString();
+        if(username.isEmpty()){
+            username_edittext.setError("Username cannot be empty!");
+            return false;
+        } else{
+            username_edittext.setError(null);
+            return true;
+        }
+    }
+
+    public boolean validateEmail(){
+        String email = email_edittext.getText().toString();
+        if(email.isEmpty()){
+            email_edittext.setError("Email cannot be empty!");
+            return false;
+        } else if(!StringHelper.dupa(email)){
+            email_edittext.setError("Email is not valid!");
+            return false;
+        } else{
+            email_edittext.setError(null);
+            return true;
+        }
+    }
+
+    public boolean validatePassword(){
+        String password = password_edittext.getText().toString();
+        String confirm_password = confirm_password_edittext.getText().toString();
+        if(password.isEmpty()){
+            password_edittext.setError("Password cannot be empty!");
+            return false;
+        } else if(confirm_password.isEmpty()){
+            confirm_password_edittext.setError("Confirm password cannot be empty!");
+            return false;
+        } else if(!password.equals(confirm_password)){
+            confirm_password_edittext.setError("Confirm password has to be the same as password!");
+            return false;
+        } else{
+            password_edittext.setError(null);
+            confirm_password_edittext.setError(null);
+            return true;
+        }
     }
 }
