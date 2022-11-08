37576e19132e3a4735b617fb24143b94017c9391
GucioWons <wgutek1@wp.pl>
2022-11-08 10:29:30 +0100

    Main activity buttons

--- app/src/main/java/com/guciowons/footballer_guesser_app/MainActivity.java
+++ app/src/main/java/com/guciowons/footballer_guesser_app/MainActivity.java
@@ -2,13 +2,28 @@ package com.guciowons.footballer_guesser_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button sign_in_btn, sign_out_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToSignIn(View view){
+        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    public void goToSignUp(View view){
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
