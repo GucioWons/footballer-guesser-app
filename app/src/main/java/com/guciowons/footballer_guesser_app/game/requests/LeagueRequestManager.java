ace6c63460b788b8fa62767aa862797a537867ce
GucioWons <wgutek1@wp.pl>
2022-11-16 17:18:28 +0100

    Leagues activity loading dialog

--- app/src/main/java/com/guciowons/footballer_guesser_app/game/requests/LeagueRequestManager.java
+++ app/src/main/java/com/guciowons/footballer_guesser_app/game/requests/LeagueRequestManager.java
@@ -8,6 +8,7 @@ import com.android.volley.Request;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.guciowons.footballer_guesser_app.game.activities.LeaguesActivity;
+import com.guciowons.footballer_guesser_app.game.activities.LoadingDialog;
 import com.guciowons.footballer_guesser_app.game.entities.League;
 import com.guciowons.footballer_guesser_app.preferences.EncryptedPreferencesGetter;
 
@@ -26,9 +27,13 @@ public class LeagueRequestManager {
     public JsonArrayRequest getLeaguesRequest(LeaguesActivity activity){
         String url = "http://192.168.0.2:8080/leagues";
         return new JsonArrayRequest(Request.Method.GET, url, null,
-                response -> activity.setLeagues(convertResponseToLeagues(response)), error -> {
-            String body = new String(error.networkResponse.data, StandardCharsets.UTF_8);
-            Toast.makeText(activity, body, Toast.LENGTH_SHORT).show();
+                response -> {
+                    activity.setLeagues(convertResponseToLeagues(response));
+                    activity.getLoadingDialog().dismissAlertDialog();
+                },
+                error -> {
+                    String body = new String(error.networkResponse.data, StandardCharsets.UTF_8);
+                    Toast.makeText(activity, body, Toast.LENGTH_SHORT).show();
         });
     }
 
