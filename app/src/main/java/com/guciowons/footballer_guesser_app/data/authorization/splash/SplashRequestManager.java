d729b1984dfd6ad5cbf2030c1a1ee7354f37ea6e
GucioWons <wgutek1@wp.pl>
2023-01-12 14:57:27 +0100

    Fixed splash login

--- app/src/main/java/com/guciowons/footballer_guesser_app/data/authorization/splash/SplashRequestManager.java
+++ app/src/main/java/com/guciowons/footballer_guesser_app/data/authorization/splash/SplashRequestManager.java
@@ -11,7 +11,7 @@ import java.nio.charset.StandardCharsets;
 
 public class SplashRequestManager {
     public JsonObjectRequest getLoginRequest(SplashViewModel model, JSONObject params){
-        String url = "http://footballerguesserservice-env.eba-iwqz7xzh.eu-central-1.elasticbeanstalk.com//";
+        String url = "http://footballerguesserservice-env.eba-iwqz7xzh.eu-central-1.elasticbeanstalk.com/login/";
         return new JsonObjectRequest(Request.Method.POST, url, params,
                 response -> model.setResponse(response, params),
                 error -> model.setErrorResponse(getErrorResponse(error)));
